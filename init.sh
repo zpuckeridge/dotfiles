b8c292d7af79986bf33617ac6ac6bb3f7e9cc85b
Zacchary Puckeridge <zacchary@puckeridge.me>
2022-08-19 11:51:27 +1000

    Added nodenv installation/setup

--- init.sh
+++ init.sh
@@ -6,6 +6,14 @@ sudo apt install zsh
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" | sh install.sh
 
+# install nodenv
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+cd ~/.nodenv && src/configure && make -C src
+~/.nodenv/bin/nodenv init
+# setup node-build
+mkdir -p "$(nodenv root)"/plugins
+git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+
 # clone dotfiles
 echo "Cloning dotfiles..."
 git clone git@github.com:zpuckeridge/dotfiles.git $HOME
